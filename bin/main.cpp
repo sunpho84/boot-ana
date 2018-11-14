91e0491da528ea2d24adc0ec6b663a54c4c7fe1d
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-11-15 00:29:52 +0100

    AAdding some bits

--- bin/main.cpp
+++ bin/main.cpp
@@ -2,7 +2,57 @@
  #include <config.hpp>
 #endif
 
+#include <map>
+#include <vector>
+
+using namespace std;
+
+/// Identify an addend
+using Addend=
+  vector<int>;
+
+/// Maps each addend to its weight
+using WeightedAddend=
+  map<Addend,int>;
+
+/// Representation of M
+constexpr int M0=
+  0;
+
+/// Representation of D
+constexpr int D1=
+  -1;
+
+/// Associate the i-th of the n^2 terms in the (D+m)^n expansion
+///
+/// Decompose the bit representation, and maps 0 to D, 1 to m
+Addend getTerm(const int i,const int n)
+{
+  /// Returned value
+  Addend ret(n);
+  
+  for(int j=0;j<n;j++)
+    {
+      /// j-th bit of i
+      const bool b=
+	(i>>j)%0x1;
+      
+      ret[j]=
+	b?M0:D1;
+    }
+  
+  return ret;
+}
+
+void simplfyAndAdd()
+{
+}
+
 int main(int narg,char **arg)
 {
+  const int n=
+    10;
+  
+  
   return 0;
 }
